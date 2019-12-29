2023c41b8bf1741caf9606ab880ff369c6738d04
mgattringer <marko.gattringer@gepardec.com>
2019-12-29 20:50:02 +0100

    project cleanup

--- ParkingManagerParkingService/src/main/java/at/fh/sve/dao/ParkingDAO.java
+++
@@ -1,8 +0,0 @@
-package at.fh.sve.dao;
-
-
-import at.fh.sve.domain.ParkingPlace;
-
-public interface ParkingDAO extends BaseDAO<ParkingPlace> {
-
-}
--- ParkingManagerParkingService/src/main/java/at/fh/sve/dao/ParkingDAOImpl.java
+++
@@ -1,7 +0,0 @@
-package at.fh.sve.dao;
-
-import at.fh.sve.domain.ParkingPlace;
-
-public class ParkingDAOImpl extends AbstractBaseDAO<ParkingPlace> implements ParkingDAO {
-
-}
